f063d716009e933a9749d0af6821f4940336aaf1
scar3crow0 <scarecrow12555@gmail.com>
2024-05-05 01:07:51 +0500

    sidebar complete, homepage complete, routing logic and page structure implemented

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import iconpath1 from "../assets/icons/meeting.png";
+import iconpath2 from "../assets/icons/stats.png";
+import iconpath3 from "../assets/icons/messages.png";
+import iconpath4 from "../assets/icons/flag.png";
+import iconpath5 from "../assets/icons/e-learning.png";
+import iconpath6 from "../assets/icons/memo-pad.png";
 
-const Sidebar = () => {
+const sidebar = () => {
   return (
-    <div className=" fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-md">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+    <div className="  flex-1 fixed top-0 left-0 gap-3 h-screen w-16 m-0 flex flex-col bg-primary text-text items-center">
+      <div className=" mt-4">
+        <img className=" w-8 h-8 " src={iconpath5} alt="Iconalt" />
+        {"Solo"}
+      </div>
+
+      <div>
+        <img className=" w-8 h-8" src={iconpath2} alt="Iconalt" />
+        {"Stats"}
+      </div>
+      <div>
+        <img className=" w-8 h-8" src={iconpath4} alt="Iconalt" />
+        {"Goals"}
+      </div>
+      <div>
+        <img className=" w-8 h-8" src={iconpath3} alt="Iconalt" />
+        {"Chats"}
+      </div>
+
+      <div>
+        <img className=" w-8 h-8" src={iconpath6} alt="Iconalt" />
+        {"Notes"}
+      </div>
+      <div>
+        <img className=" w-8 h-8" src={iconpath1} alt="Iconalt" />
+        {"Group"}
+      </div>
     </div>
   );
 };
 
-const SidebarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
+// const SidebarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
 
-export default Sidebar;
+export default sidebar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#40095A",
+      text: "#F6F7F9",
+      secondary: "#7E4396",
+      background_default: "#180118",
+      secondary2: "#731042",
+    },
   },
   plugins: [],
 };
