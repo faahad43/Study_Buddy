b5d886e937dcb2c94b6d15975ed00478d71ebcdd
web-proj9 <aliabdulllah615@gmail.com>
2024-05-11 15:57:00 +0500

    login and sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import SoloStudyPage from "./pages/SoloStudy.jsx";
 import StudyGroupPage from "./pages/GroupStudy.jsx";
 import StatsPage from "./pages/Stats.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import Login from "./pages/Login.jsx";
+import Registration from "./pages/Registration.jsx";
 
 function App() {
   return (
     <Router>
       <div>
-        <Sidebar />
+        {/* <Sidebar /> */}
         <Routes>
           <Route path="/solo" element={<SoloStudyPage />} />
           <Route path="/group" element={<StudyGroupPage />} />
           <Route path="/stats" element={<StatsPage />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          
         </Routes>
       </div>
     </Router>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import { blue } from '@mui/material/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,6 +12,18 @@ export default {
       secondary: "#7E4396",
       background_default: "#180118",
       secondary2: "#731042",
+      blue5: "#3b82f6",
+      blue6: "#2563eb",
+      emerald8: "#065f46",
+      emerald6: "#059669",
+      gray9: "#111827",
+      gray6: "#4b5563",
+      gray3: "#d1d5db",
+      slate9: "#0f172a",
+      slate8: "#1e293b",
+      slate6: "#475569",
+      slate4: "#94a3b8"
+
     },
   },
   plugins: [],
